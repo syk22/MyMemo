4e6891f5735141e39456fe6c3aee96d0e5943353
SayakaNakajima <andsayaka@gmail.com>
2023-06-27 14:12:35 +0900

    Preparation for componentization

--- src/components/ToDoTasks.tsx
+++ src/components/ToDoTasks.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 // import { Header } from './Header';
 import { ToDoFormat } from '../types/ToDoFormat';
 import { List } from './List';
+import '../assets/styles/style.css';
 
 let idCount = 0;
 
@@ -11,9 +12,7 @@ export const ToDoTasks = () => {
   const returnNowTime = () => new Date();
   const createTodo = () => {
     idCount++;
-    const getAddTodoText: HTMLInputElement = document.getElementById(
-      'new-add-todo'
-    ) as HTMLInputElement;
+    const getAddTodoText: HTMLInputElement = document.getElementById('new-add-todo') as HTMLInputElement;
     const addTodoText: string | undefined = getAddTodoText?.value;
     if (addTodoText !== undefined) {
       const addTodo: ToDoFormat = {
@@ -25,7 +24,7 @@ export const ToDoTasks = () => {
         isModifing: false,
         createdDateTime: returnNowTime(),
       };
-      if (todoArray.length > 0) setTodoArray(prev => [...prev, addTodo]);
+      if (todoArray.length > 0) setTodoArray((prev) => [...prev, addTodo]);
       else setTodoArray([addTodo]);
     }
     getAddTodoText.value = '';
@@ -37,9 +36,23 @@ export const ToDoTasks = () => {
       <br />
       <input type="input" id="new-add-todo" />
       <button onClick={createTodo}>add task</button>
-      <ul>
+      <ul className="todo-lists">
         {todoArray !== undefined && todoArray?.length > 0
-          ? todoArray.map(v => {
+          ? todoArray.map((v) => {
+              return <List key={v.id.toString()} {...v} />;
+            })
+          : null}
+      </ul>
+      <ul className="done-lists">
+        {todoArray !== undefined && todoArray?.length > 0
+          ? todoArray.map((v) => {
+              return <List key={v.id.toString()} {...v} />;
+            })
+          : null}
+      </ul>
+      <ul className="remove-lists">
+        {todoArray !== undefined && todoArray?.length > 0
+          ? todoArray.map((v) => {
               return <List key={v.id.toString()} {...v} />;
             })
           : null}
