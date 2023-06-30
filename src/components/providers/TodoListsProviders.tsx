a38860695645cefbbc11f204a98bd9875e35a70b
SayakaNakajima <andsayaka@gmail.com>
2023-06-30 13:47:12 +0900

    Added function for finished todo.

--- src/components/providers/TodoListsProviders.tsx
+++ src/components/providers/TodoListsProviders.tsx
@@ -1,14 +1,16 @@
 import { createContext, useState } from 'react';
 import { TodoFormat } from '../../types/TodoFormat';
-import { TodoListType } from '../../types/ProviderTodoListType';
+import { TodoActionType, TodoListType } from '../../types/ProviderTodoListType';
 
 let idCount = 0;
 
 export const TodoListsContext = createContext({} as TodoListType);
+export const TodoActionContext = createContext({} as TodoActionType);
 export const TodoListsProvider = (props: any) => {
   const { children } = props;
   const [todoList, setTodoList] = useState([] as TodoFormat[]);
   const [todoDone, setTodoDone] = useState([] as TodoFormat[]);
+  const [todoClosed, setTodoClosed] = useState([] as TodoFormat[]);
   const returnNowTime = () => new Date();
 
   const createTodo = () => {
@@ -40,9 +42,20 @@ export const TodoListsProvider = (props: any) => {
     setTodoList(revTodoList);
   };
 
+  const closedTodo = (index: number) => {
+    const addTodoClosed = todoList.find((v) => v.id === index);
+    if (addTodoClosed !== undefined) {
+      addTodoClosed.closedDateTime = returnNowTime();
+      const revTodoClosed = todoClosed.length > 0 ? [...todoClosed, addTodoClosed] : [addTodoClosed];
+      setTodoClosed(revTodoClosed);
+    }
+    const revTodoList = todoList.filter((v) => v.id !== index);
+    setTodoList(revTodoList);
+  };
+
   return (
-    <TodoListsContext.Provider value={{ todoList, todoDone, createTodo, doneTodo }}>
-      {children}
-    </TodoListsContext.Provider>
+    <TodoActionContext.Provider value={{ createTodo, doneTodo, closedTodo }}>
+      <TodoListsContext.Provider value={{ todoList, todoDone, todoClosed }}>{children}</TodoListsContext.Provider>
+    </TodoActionContext.Provider>
   );
 };
--- src/types/ProviderTodoListType.ts
+++ src/types/ProviderTodoListType.ts
@@ -4,6 +4,10 @@ import { TodoFormat } from './TodoFormat';
 export type TodoListType = {
   todoList: TodoFormat[];
   todoDone: TodoFormat[];
+  todoClosed: TodoFormat[];
+};
+export type TodoActionType = {
   createTodo: MouseEventHandler;
   doneTodo: Function;
+  closedTodo: Function;
 };
